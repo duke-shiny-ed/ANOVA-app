0c901e888f27c6e44ad83385cedd28983fc0e544
Samantha Owusu-Antwi <spo8@duke.edu>
2020-07-08 02:23:02 +0000

    User Interface Code

--- app.R
+++ app.R
@@ -1,20 +1,89 @@
 library(shiny)
 
-ui <- fluidPage(
-    navbarPage(title = "ANOVA",
+ui <- navbarPage(title = "ANOVA",
         tabPanel("About",
-                tags$h1("About the App"),
-                tags$br(),
-                tags$p("contents")
-                ),
+                    tags$h1("About the App"),
+                    tags$br(),
+                    tags$p("contents")),
         tabPanel("Robustness of Assumptions",
-                ),
-        tabPanel("Relationship between ANOVA and F-statistic")
-        )
+                    sidebarLayout(
+                            sidebarPanel(
+                            fluidRow(
+                            selectInput(inputId = "skew", 
+                                        label = "Shape",
+                                        choices = c("Normal",
+                                                    "Right Skewed",
+                                                    "Left Skewed",
+                                        selected = "Normal")
+                                        ),
+                            tags$br(),
+                            tags$p("Instructions")),
+                            
+                            fluidRow(tags$h4("Whithin group variance"),
+                                     tags$p("Instructions"),
+                                     tags$br(),
+                                     
+                                     sliderInput(inputId = "sd1",
+                                                 label = p("Curve 1", style = "color:red"),
+                                                 min = 0.5, max = 2, value = 1, step = 0.5),
+                                     tags$br(),
+                                     sliderInput(inputId = "sd2",
+                                                 label = p("Curve 2", style = "color:blue"),
+                                                 min = 0.5, max = 2, value = 1, step = .05),
+                                     tags$br(),
+                                     sliderInput(inputId = "sd3",
+                                                 label = p("Curve 3", style = "color:green"),
+                                                 min = 0.5, max = 2, value = 1, step = .05))),
+                    mainPanel(
+                    fluidRow(
+                        column(plotOutput(outputId = "curve"), width = 8),
+                        column(verbatimTextOutput(outputId = "aovTest"), width = 4,
+                               tags$br(),
+                               textOutput(outputId = "concl"))))
+                )
+             ),
+        tabPanel("Relationship between ANOVA and F-Statistic",
+                sidebarLayout(
+                    sidebarPanel(tags$h4("Whithin group variance"),
+                                tags$p("Instructions"),
+                                tags$br(),
+                                         
+                                sliderInput(inputId = "sd1",
+                                            label = p("Curve 1", style = "color:red"),
+                                            min = 0.5, max = 2, value = 1, step = 0.5),
+                                tags$br(),
+                                sliderInput(inputId = "sd2",
+                                            label = p("Curve 2", style = "color:blue"),
+                                            min = 0.5, max = 2, value = 1, step = .05),
+                                tags$br(),
+                                sliderInput(inputId = "sd3",
+                                            label = p("Curve 3", style = "color:green"),
+                                            min = 0.5, max = 2, value = 1, step = .05),
+                                tags$br(),
+                                tags$br(),
+                                         
+                                sliderInput(inputId = "btwsd",
+                                            label = "Between group variance",
+                                            min = 1, max = 5, value = 1),
+                                tags$br(),
+                                tags$p("Instructions"))),
+                    mainPanel(
+                        fluidRow(
+                            column(plotOutput(outputId = "boxplot"), width = 8),
+                            column(tags$p("Non-reactive text explaining what F-stat is..."),
+                                    tags$br(),
+                                    verbatimTextOutput(outputId = "aovTest"),
+                                    tags$br(),
+                                    tags$br(),
+                                    
+                                   textOutput(outputId = "concl"), width = 4)))
+               
     )
+                
+)
 
-server <- function(input, output) {
-}
+
+server <- function(input, output) {}
 
 
 shinyApp(ui = ui, server = server)
